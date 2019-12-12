9c9e011666905d1e21198a9f4b12be934dcdd8ac
Maxwell Farver <mfarver99@gmail.com>
2019-12-11 19:52:34 -0600

    Fixing Insert

--- ProjectInsert.sql
+++ ProjectInsert.sql
@@ -1,10 +1,8 @@
 use political_tweets;
 
-SHOW VARIABLES LIKE "secure_file_priv";
-
 SET FOREIGN_KEY_CHECKS = 0;
 
-Load Data Infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/user.txt'
+Load Data Infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/user.csv'
 Into Table user
 Fields Terminated By ';' Optionally Enclosed By '"'
 Lines Terminated By '\n'
@@ -14,33 +12,35 @@ Ignore 1 Lines
 Load Data Infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/tweets.csv'
 Into Table tweets
 Fields Terminated By ';' Optionally Enclosed by '"'
-Lines Terminated By '\n'
+Lines Terminated By '\r\n'
 Ignore 1 Lines
-(tid, textbody, retweet_count, retweeted, @col5, posting_user)
+(tid, textbody, retweet_count, retweeted, @posted, posting_user)
 set theDay = day(STR_TO_DATE(@posted,'%Y-%m-%d %H:%i:%s')),
 	theMonth =  month(STR_TO_DATE(@posted,'%Y-%m-%d %H:%i:%s')),
 	theYear =  year(STR_TO_DATE(@posted,'%Y-%m-%d %H:%i:%s')),
 	created_at = UNIX_TIMESTAMP(@posted);
 
 
-Load Data Infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/tagged.txt'
+Load Data Infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/tagged.csv'
 Into Table tagged
 Fields Terminated By ';'
 Lines Terminated By '\n'
 Ignore 1 Lines
 (tid, hashtagname);
 
-Load Data Infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/mentioned.txt'
+Load Data Infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/mentioned.csv'
 Into Table mentioned
-Fields Terminated By ';'
+Fields Terminated By ';' Optionally Enclosed By '"'
+Lines Terminated By '\r\n'
 Ignore 1 Lines
 (tid, screen_name);
 
-Load Data Infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/urlused.txt'
+Load Data Infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/urlused.csv'
 Into Table urlused
 Fields Terminated By ';'
 Lines Terminated By '\n'
 Ignore 1 Lines
 (tid, url);
+
 
 
