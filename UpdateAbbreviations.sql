70b4f87a56635060b6cd0a5b15ebd8105201af96
tdpotter58 <36381253+tdpotter58@users.noreply.github.com>
2019-12-13 17:54:32 -0600

    Create UpdateAbbreviations.sql

---
+++ UpdateAbbreviations.sql
@@ -0,0 +1,29 @@
+use political_tweets;
+
+UPDATE user
+SET ofstate = 'Florida'
+WHERE ofstate = 'FL';
+
+UPDATE user
+SET ofstate = 'Arizona'
+WHERE ofstate = 'AZ';
+
+UPDATE user
+SET ofstate = 'South Dakota'
+WHERE ofstate = 'SD';
+
+UPDATE user
+SET ofstate = 'Iowa'
+WHERE ofstate = 'IA';
+
+UPDATE user
+SET ofstate = 'Illinois'
+WHERE ofstate = 'IL';
+
+UPDATE user
+SET ofstate = 'Wisconsin'
+WHERE ofstate = 'WI';
+
+UPDATE user
+SET ofstate = 'Nebraska'
+WHERE ofstate = 'NE';
