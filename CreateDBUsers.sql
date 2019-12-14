2c1a637ce1602cc789e87251ec0e1df174a21045
Maxwell Farver <mfarver99@gmail.com>
2019-12-13 22:39:56 -0600

    update dbuser

--- CreateDBUsers.sql
+++ CreateDBUsers.sql
@@ -1,4 +1,3 @@
 CREATE USER 'cs363'@'%1' IDENTIFIED WITH mysql_native_password BY 'speedIsAnUnderratedMovie';
-GRANT SELECT, DROP, CREATE, INSERT, DELETE ON political_tweets.* TO 'cs363'@'%1';
-GRANT TRIGGER ON political_tweets.* TO 'cs363'@'%1';
+GRANT ALL ON group5.* TO 'cs363'@'%1';
 FLUSH PRIVILEGES;
