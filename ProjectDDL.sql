fb32cc82dacb4c210f6ba131e832b565126fde98
Maxwell Farver <mfarver99@gmail.com>
2019-12-13 22:46:09 -0600

    update ddl

--- ProjectDDL.sql
+++ ProjectDDL.sql
@@ -1,6 +1,6 @@
-DROP SCHEMA IF EXISTS political_tweets;
-CREATE SCHEMA political_tweets;
-USE political_tweets;
+DROP SCHEMA IF EXISTS group5;
+CREATE SCHEMA group5;
+USE group5;
 
 CREATE TABLE user (
   screen_name VARCHAR(80),
@@ -22,29 +22,29 @@ CREATE TABLE tweets (
   theMonth int,
   theYear int,
   created_at bigint,
-  posting_user VARCHAR(400) NOT NULL,
-  foreign key (posting_user) references user(screen_name),
+  posting_user VARCHAR(80) NOT NULL,
+  foreign key (posting_user) references user(screen_name) on delete cascade,
   PRIMARY KEY  (tid)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE tagged (
   tid varchar(50),
   hashtagname VARCHAR(80),
-  foreign key (tid) references tweets(tid),
+  foreign key (tid) references tweets(tid) on delete cascade,
   PRIMARY KEY  (tid, hashtagname)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE mentioned (
   tid varchar(50),
   screen_name VARCHAR(80),
-  foreign key (tid) references tweets(tid),
-  foreign key (screen_name) references user(screen_name),
+  foreign key (tid) references tweets(tid) on delete cascade,
+  foreign key (screen_name) references user(screen_name) on delete cascade,
   PRIMARY KEY  (tid, screen_name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE urlused (
   tid varchar(50),
   url VARCHAR(500),
-  foreign key (tid) references tweets(tid),
+  constraint fk_tweet_url foreign key (tid) references tweets(tid) on delete cascade,
   PRIMARY KEY  (tid, url)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
