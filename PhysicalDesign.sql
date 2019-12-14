294f685d0f612a605c325b28c6f6eee3c4486a89
tdpotter58 <36381253+tdpotter58@users.noreply.github.com>
2019-12-13 20:21:32 -0600

    Create PhysicalDesign.sql

---
+++ PhysicalDesign.sql
@@ -0,0 +1,22 @@
+use political_tweets;
+
+create index q1 on tweets(theYear);
+select * from tweets where theYear = 2016;
+
+drop index q1 on tweets;
+
+create index dem on user(sub_category);
+select * from user where sub_category like '%GOP%';
+
+create index debate on tagged(hashtagname);
+select * from tagged where hashtagname like '%Debate%';
+
+create index ohio on user(ofstate);
+select * from user where ofstate like '%Ohio%';
+
+create index u on user(screen_name);
+
+drop index dem on user;
+drop index debate on tagged;
+drop index ohio on user
+drop index u on user;
